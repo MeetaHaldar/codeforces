d475ce4cefd740a19c571015f779ba2a5c7469b2
meeta haldar <meetahaldar1001@gmail.com>
2021-06-26 23:16:05 +0530

    question completed

--- 263A.cpp
+++ 263A.cpp
@@ -1,15 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
 
-int a[5][5];
-for (int i = 1; i <=5; i++)
-{
-    cin>>a[i][1]>>a[i][2]>>a[i][3]>>a[i][4]>>a[i][5];
+    int a[5][5], s = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cin >> a[i][j];
 
-}
+            if (a[i][j] == 1)
+            {
+// abs is a function for making negative integer to positive integer.
 
-
-
+                s = abs(i - 2) + abs(j - 2);
+            }
+        }
+    }
+    cout << s;
+    return 0;
 }
