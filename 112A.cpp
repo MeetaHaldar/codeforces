1cd4ebc6c857fbfabe50b2be66240a1b07d8905d
meeta haldar <meetahaldar1001@gmail.com>
2021-06-28 00:08:38 +0530

    comments

---
+++ 112A.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    int c = a[a.length() - 1];
+    int d = b[b.length() - 1];
+    // if (a[a.length() - 1] > 96 && b[b.length() - 1] > 96)
+    //     {
+    //         if (a[a.length() - 1] < b[b.length() - 1])
+    //     {
+    //         cout << "-1";
+    //     }
+    //     else if (a[a.length() - 1] > b[b.length() - 1])
+    //     {
+    //         {
+    //             cout << "+1";
+    //         }
+         
+    //     }
+    //        else
+    //         {
+    //             cout << "0";
+    //         }
+    //     }
+    // else if (a[a.length() - 1] < 96 && b[b.length() - 1] < 96)
+    // {
+    //     if (a[a.length() - 1] < b[b.length() - 1])
+    //     {
+    //         cout << "-1";
+    //     }
+    //     else if (a[a.length() - 1] > b[b.length() - 1])
+    //     {
+    //         {
+    //             cout << "+1";
+    //         }
+         
+    //     }
+    //        else
+    //         {
+    //             cout << "0";
+    //         }
+    // }
+     if (a[a.length() - 1] > 96)
+    {
+        c = c - 32;
+    }
+   if (b[b.length() - 1] > 96)
+        d = d - 32;
+
+    if (c == d)
+    {
+        cout << "0";
+    }
+    else if (c > d)
+    {
+        cout << "1";
+    }
+
+    else
+    {
+        cout << "-1";
+    }
+
+    return 0;
+}
