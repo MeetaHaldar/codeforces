36042e9a3eafe7d779f02554b222652f96246bad
meeta haldar <meetahaldar1001@gmail.com>
2021-06-27 22:55:29 +0530

    question completed

---
+++ 282A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, X = 0;
+    string p;
+    cin >> n;
+    for (; n > 0; n--)
+    {
+        cin >> p;
+        if (p[1] == '+')
+        {
+            ++X;
+        }
+
+        else
+        {
+            --X;
+        }
+    }
+    cout << X;
+
+    return 0;
+}
