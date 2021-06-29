f503f255001749c8737baa8e71ffa076529f8ed2
meeta haldar <meetahaldar1001@gmail.com>
2021-06-30 00:10:39 +0530

    completd

---
+++ 110A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string a;
+    int count = 0;
+    cin >> a;
+    for (int i = 0; i < a.length(); i++)
+    {
+
+        if (a[i] == '4' || a[i] == '7')
+        {
+            count++;
+        }
+    }
+
+    if (count == 7 || count == 4)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    return 0;
+}
