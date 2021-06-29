828d824375ce3a9be3148ae21699c1d478f50810
meeta haldar <meetahaldar1001@gmail.com>
2021-06-30 00:58:03 +0530

    completed

---
+++ 734A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, c1 = 0, c2 = 0;
+    string a;
+    cin >> n;
+    cin >> a;
+    for (int i = 0; i < n; i++)
+
+    {
+        if (a[i] == 'A')
+        {
+            c1++;
+        }
+        else if (a[i] == 'D')
+        {
+            c2++;
+        }
+    }
+
+    if (c1 > c2)
+    {
+        cout << "Anton";
+    }
+    else if (c2 > c1)
+    {
+        cout << "Danik";
+    }
+    else if (c1 == c2)
+    {
+        cout << "Friendship";
+    }
+    return 0;
+}
