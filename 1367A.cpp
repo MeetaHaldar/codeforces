fbaa5ab1cb1ecfe8a0e0d5eaa453af96cc4e9aa0
meeta haldar <meetahaldar1001@gmail.com>
2021-07-09 22:43:39 +0530

    completed 1367A

--- 1367A.cpp
+++ 1367A.cpp
@@ -9,14 +9,17 @@ int main()
     for (int i = 0; i < t; i++)
     {
         cin >> a;
-        for (int i = 0; i < a.length(); i++)
+        cout << a[0];
+
+        if (a.length() > 2)
         {
-            if(a[i]==a[i+1])
+            for (int i = 1; i < a.length() - 1; i += 2)
             {
-                cout<<a[i];
+
+                cout << a[i];
             }
-            
         }
+        cout << a[a.length() - 1] << endl;
     }
 
     return 0;
---
+++ 472A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,x,y;
+    cin>>n;
+
+
+
+
+
+
+    
+cout<<x <<y;
+    return 0;
+}
